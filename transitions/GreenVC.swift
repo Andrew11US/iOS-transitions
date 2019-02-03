4c9a271b2e55cd22b07349601a551950d89d670d
Andrii Halabuda <losAngelesBoy11@gmail.com>
2019-02-03 20:16:20 +0100

    Added files

---
+++ transitions/GreenVC.swift
@@ -0,0 +1,21 @@
+//
+//  GreenVC.swift
+//  transitions
+//
+//  Created by Andrew on 2/3/19.
+//  Copyright © 2019 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+class GreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
--- transitions/ViewController.swift
+++ transitions/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Hero
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func goToGreen(_ sender: UIButton) {
+        let greenVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "VC2") as! ViewController
+        greenVC.isHeroEnabled = true
+        greenVC.heroModalAnimationType = .zoomSlide(direction: HeroDefaultAnimationType.Direction.left)
+        self.hero_replaceViewController(with: greenVC)
+    }
+    
+    @IBAction func goRedPressed(_ sender: Any) {
+        let redVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "VC1") as! ViewController
+        redVC.isHeroEnabled = true
+        redVC.heroModalAnimationType = .zoomSlide(direction: HeroDefaultAnimationType.Direction.right)
+        self.hero_replaceViewController(with: redVC)
+    }
+    
 }
 
